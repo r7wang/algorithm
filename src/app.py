a3fdd396dbac24ffd2e9443aa174f42da3fdb249
Ray Wang <r7wang@gmail.com>
2020-04-23 14:44:47 -0400

    Add implementation for 2d array operations.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,9 @@
-from src.heap import build
+from src import array
 
-heap = build()
-
-for _ in range(0, 2):
-    item1 = heap.pop()
-    item2 = heap.pop()
-    print('Popped: item1={} item2={}'.format(item1, item2))
-    diff = item1 - item2
-    heap.insert(diff)
-    print('Inserted: {}'.format(diff))
+# arr = array.make_2d_array_yx_seeded_col_order_alternating_using_iter([1, 3, 4, 7, 6, 2, 9, 5], 3, 3)
+# arr = array.make_2d_array_yx_seeded_col_order_alternating_using_inf(5, 5)
+arr = array.make_2d_array_yx_unseeded_col_order_alternating_using_inf(5, 5)
+arr2 = array.copy_2d_array(arr)
+array.mult_2d_array_in_place(arr, 3)
+array.print_2d_yx(arr)
+array.print_2d_yx(arr2)
