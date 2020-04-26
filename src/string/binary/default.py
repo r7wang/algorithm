05ab8170a52c60f779cb17697f7c5ae65d0fa5dd
Ray Wang <r7wang@gmail.com>
2020-04-26 14:46:31 -0400

    Add implementation for text to binary.

---
+++ src/string/binary/default.py
@@ -0,0 +1,20 @@
+def to_binary(text: str) -> None:
+    for letter in text:
+        print(_binary(letter))
+
+
+def _binary(letter: str) -> str:
+    result = []
+    ordinal = ord(letter)
+
+    # O(n^2), where n is number of bits required to store a letter
+    while ordinal > 0:
+        if (ordinal >> 1 << 1) != ordinal:
+            result.append('1')
+        else:
+            result.append('0')
+        ordinal = ordinal >> 1
+
+    # O(n)
+    result.reverse()
+    return ''.join(result)
