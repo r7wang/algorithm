be605e69cda43bab7e2cf37f68f0612af4af2fd0
Ray Wang <r7wang@gmail.com>
2020-05-07 14:00:00 -0400

    Add implementation for friend matcher.

--- src/problem/friend_matcher/__init__.py
+++ src/problem/friend_matcher/__init__.py
@@ -13,3 +13,52 @@ Build a class that has the two following methods:
 Notes:
     - Design the system to handle up to 1000 users.
 """
+import random
+from collections import defaultdict
+from typing import List, Tuple
+
+
+class FriendMatcher:
+    """
+    Strategy 1:
+        - Pop random item from list to select every user, but cost is O(n) to select a user.
+    """
+    users = []
+    history = defaultdict(set)
+
+    def add_user(self, name: str) -> None:
+        self.users.append(name)
+
+    def suggest_matches(self) -> List[Tuple[str, str]]:
+        matches = []
+        remaining_users = list(self.users)
+        while len(remaining_users) >= 2:
+            # Get first user.
+            user_a = remaining_users[0]
+
+            # Generate potential matches.
+            potential_matches = []
+            for user_idx in range(1, len(remaining_users)):
+                user_b = remaining_users[user_idx]
+                if user_b in self.history[user_a]:
+                    continue
+                potential_matches.append(user_b)
+
+            # Get second user.
+            if len(potential_matches) == 0:
+                remaining_users.remove(user_a)
+                continue
+
+            user_idx = random.randint(0, len(potential_matches) - 1)
+            user_b = potential_matches[user_idx]
+
+            # Add history.
+            self.history[user_a].add(user_b)
+            self.history[user_b].add(user_a)
+
+            # Update remaining users.
+            remaining_users.remove(user_a)
+            remaining_users.remove(user_b)
+
+            matches.append((user_a, user_b))
+        return matches
