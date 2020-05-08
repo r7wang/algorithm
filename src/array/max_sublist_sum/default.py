cdce3f51d740c02946cec23119b1da4e1432802b
Ray Wang <r7wang@gmail.com>
2020-05-08 09:16:54 -0400

    Improve maximum sublist sum solution to linear time with explanation.

--- src/array/max_sublist_sum/default.py
+++ src/array/max_sublist_sum/default.py
@@ -1,12 +1,52 @@
 """
 Find the maximum sublist sum of an array containing positive and negative integers.
 """
-from typing import List
+from typing import List, Optional
+
+NO_SOLUTION = -999
 
 
-def max_sublist_sum(data: List[int]) -> int:
+def max_sublist_sum(data: List[int]) -> Optional[int]:
     # return _solve_brute_force(data)
-    return _solve_iterative(data)
+    # return _solve_iterative(data)
+    return _solve_linear(data)
+
+
+def _solve_linear(data: List[int]) -> Optional[int]:
+    # For the whole array from 0 -> (n-1):
+    #   - If index 0 is negative, we don't want that element.
+    #   - If index 0 is positive, and sum(index 0 -> k) is negative, then item k must be sufficiently negative to wipe
+    #     out positivity from 0 -> k-1.
+    #
+    # Index: 0                    k
+    #        P------------------>PN
+    #        ----------------------
+    #
+    # Notes:
+    #   - There is no way for sum(index 1 -> k-1) to be more positive than what k would wipe out, because that would
+    #     imply index 0 to be negative, and if index 0 is negative, there's no reason to start accumulating from that
+    #     index.
+    #   - During the time when we're traversing the array, we should record our best solution.
+    #
+    # If we want to track the indices of the best solution:
+    #   - Track the start index of the current solution.
+    #   - Every time we update the best solution, the best solution goes from start index of the current solution to
+    #     current index.
+    best_solution = NO_SOLUTION
+    cur_solution = 0
+    for val in data:
+        cumulative_sum = cur_solution + val
+
+        # Update the best solution.
+        best_solution = max(best_solution, cumulative_sum)
+
+        # Decide whether or not to reset the current solution.
+        if cumulative_sum < 0:
+            cur_solution = 0
+        else:
+            cur_solution = cumulative_sum
+
+    return best_solution if best_solution != NO_SOLUTION else None
 
 
 def _solve_iterative(data: List[int]) -> int:
